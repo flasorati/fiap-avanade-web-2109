c849e94c819588a39486db1ada999fb6429d5f9b
Flávia Sorati <f.de.souza.pereira@avanade.com>
2022-09-29 09:36:05 -0300

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//trabalhando com JavaScript em um arquivo externo. exemplos de hoisting
+
+console.log(nome, "chamando variável criada com var");
+
+var nome = "Fiap";
+
+escreve('Exemplo de função nominal');
+
+//função nominal -udf
+
+function escreve(título){
+    alert(título);
+}
+
